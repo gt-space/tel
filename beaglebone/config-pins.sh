1ec48e01b099a242ee8c80123873204e6a5f8e7e
Justin Stephens <justephens@proton.me>
2024-02-23 18:33:11 -0500

    Config pin script for beaglebone

---
+++ beaglebone/config-pins.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Execute on a BeagleBone Black with no overlays loaded.
+# These commands require the default pin configuration in order to work.
+ 
+# For SPI1, /dev/spidev1.#
+#
+config-pin p9_17 spi_cs
+config-pin p9_18 spi
+config-pin p9_21 spi
+config-pin p9_22 spi_sclk
+ 
+# For SPI0, /dev/spidev2.#
+#
+config-pin p9_28 spi_cs
+config-pin p9_29 spi
+config-pin p9_30 spi
+config-pin p9_31 spi_sclk
---
+++ beaglebone/readme.md
@@ -0,0 +1,3 @@
+# BeagleBone Setup
+
+Follow the guide here: https://arcanesciencelab.wordpress.com/2020/01/14/the-correct-way-to-enable-spi-ports-on-the-beaglebone-black/
