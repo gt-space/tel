70885fedd73981168b92b1fff8b52b5c6b1e99ea
David Pang <dpang33@gatech.edu>
2024-03-02 17:25:10 -0500

    Add support for async

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod gpio;
 
 use std::io;
 use std::rc::Rc;
+use futures::executor::block_on;
 use spidev::{Spidev, SpidevOptions, SpiModeFlags};
 
 fn create_spi() -> io::Result<Spidev> {
@@ -21,10 +22,15 @@ fn create_spi() -> io::Result<Spidev> {
     Ok(spi)
 }
 
-fn main() {
+async fn async_main() {
     let spi = Rc::new(create_spi().unwrap());
     let gpio1 = gpio::Gpio::open(1);
     let mut device1 = spi_wrapper::SpiWrapper::new(spi, gpio1.get_pin(16));
     let mut rx_buf = [0_u8; 6];
-    let _ = device1.read_write(&mut rx_buf, &[0x19, 0x09, 0x2B, 0x00, 0x00, 0x00]);
+    let _ = device1.read_write(&mut rx_buf, &[0x19, 0x09, 0x2B, 0x00, 0x00, 0x00]).await;
+    println!("rx_buf1: {:?}", rx_buf);
+}
+
+fn main() {
+    block_on(async_main());
 }
--- src/spi_wrapper/mod.rs
+++ src/spi_wrapper/mod.rs
@@ -2,12 +2,13 @@ use spidev::{Spidev, SpidevTransfer};
 
 use std::io;
 use std::rc::Rc;
+use std::sync::Mutex;
 
 use crate::gpio::{Pin, PinMode::{Output}, PinValue::{High, Low}};
 
 pub struct SpiWrapper {
     pub spidev: Rc<Spidev>,
-    pub gpio_pin: Pin
+    pub gpio_pin: Mutex<Pin> // only allow single read/write to a pin at a time
 }
 
 impl SpiWrapper {
@@ -17,30 +18,32 @@ impl SpiWrapper {
         gpio_pin.digital_write(High);
         return SpiWrapper {
             spidev: spidev,
-            gpio_pin: gpio_pin
+            gpio_pin: Mutex::new(gpio_pin)
         };
     }
 
-    pub fn read(&mut self, rx_buf: &mut [u8]) -> io::Result<()> {
+    pub async fn read(&mut self, rx_buf: &mut [u8]) -> io::Result<()> {
         let mut transfer = SpidevTransfer::read(rx_buf);
         self.transfer(&mut transfer)
     }
 
-    pub fn write(&mut self, tx_buf: &[u8]) -> io::Result<()> {
+    pub async fn write(&mut self, tx_buf: &[u8]) -> io::Result<()> {
         let mut transfer = SpidevTransfer::write(&tx_buf);
         self.transfer(&mut transfer)
     }
 
-    pub fn read_write(&mut self, rx_buf: &mut [u8], tx_buf: &[u8]) -> io::Result<()> {
+    pub async fn read_write(&mut self, rx_buf: &mut [u8], tx_buf: &[u8]) -> io::Result<()> {
         let mut transfer = SpidevTransfer::read_write(&tx_buf, rx_buf);
         self.transfer(&mut transfer)
     }
 
     fn transfer(&mut self, transfer: &mut SpidevTransfer) -> io::Result<()> {
-        self.gpio_pin.digital_write(Low);
+        // MutexGuard pin unlocked once out of scope
+        let pin = self.gpio_pin.lock().unwrap();
+        pin.digital_write(Low);
         let result = self.spidev.transfer(transfer);
         // need to set back to high even if error in transfer
-        self.gpio_pin.digital_write(High);
+        pin.digital_write(High);
         return result
     }
 }
