d8633d0e1be1f7502989f4a8049351ae3d13593a
Tymur Tkachenko <ttkachenko3@gatech.edu>
2024-07-27 21:37:20 -0400

    GPS testing

--- src/main.rs
+++ src/main.rs
@@ -46,41 +46,68 @@ fn main() {
     let mut spi = create_spi().unwrap();
     // println!("{:?}", half_duplex(&mut spi).unwrap());
     // println!("{:?}", full_duplex(&mut spi).unwrap());
-    gpio::set_output("49");
-    gpio::set_high("49"); // power
+    
+    gpio::set_output("15");
+    gpio::set_high("15"); // 3V3-GPS
 
-    gpio::set_output("9");
-    gpio::set_low("9"); // NRESET
+    gpio::set_output("45");
+    gpio::set_low("45"); // GPS-NRESET
     thread::sleep(Duration::from_millis(100));
-    gpio::set_high("9");
+    gpio::set_high("45");
 
     gpio::set_output("72"); // LF-CS
     gpio::set_high("72");
-    gpio::set_output("44"); // GPS-CS
-    gpio::set_high("44");
     gpio::set_output("81"); // HF-CS
-
-    let mut tx_buf = [0x19, 0x08, 0x91, 0x00, 0x00, 0x00];
-    let mut rx_buf = [0; 6];
-
-    gpio::set_low("81");
-    thread::sleep(Duration::from_micros(200));
-    println!("{:?}", full_duplex(&mut spi, &mut tx_buf, &mut rx_buf).unwrap());
     gpio::set_high("81");
-    thread::sleep(Duration::from_millis(100));
+    gpio::set_output("44"); // GPS-CS
+    
+    let mut tx_buf = [0xFD, 0xFE];
+    let mut rx_buf = [0; 2];
 
-    let mut tx_buf_wr = [0x18, 0x08, 0x91, 0x35];
-    let mut rx_buf_wr = [0; 4];
-    gpio::set_low("81");
-    thread::sleep(Duration::from_micros(200));
-    println!("{:?}", full_duplex(&mut spi, &mut tx_buf_wr, &mut rx_buf_wr).unwrap());
-    gpio::set_high("81");
-    thread::sleep(Duration::from_millis(100));
     loop {
-        gpio::set_low("81");
+        gpio::set_low("44");
         thread::sleep(Duration::from_micros(200));
         println!("{:?}", full_duplex(&mut spi, &mut tx_buf, &mut rx_buf).unwrap());
-        gpio::set_high("81");
+        gpio::set_high("44");
         thread::sleep(Duration::from_millis(100));
     }
 }
+
+
+    // gpio::set_output("49");
+    // gpio::set_high("49"); // 3V3-RX
+
+    // gpio::set_output("9");
+    // gpio::set_low("9"); // HF-NRESET
+    // thread::sleep(Duration::from_millis(100));
+    // gpio::set_high("9");
+
+    // gpio::set_output("72"); // LF-CS
+    // gpio::set_high("72");
+    // gpio::set_output("44"); // GPS-CS
+    // gpio::set_high("44");
+    // gpio::set_output("81"); // HF-CS
+
+    // let mut tx_buf = [0x19, 0x08, 0x91, 0x00, 0x00, 0x00];
+    // let mut rx_buf = [0; 6];
+
+    // gpio::set_low("81");
+    // thread::sleep(Duration::from_micros(200));
+    // println!("{:?}", full_duplex(&mut spi, &mut tx_buf, &mut rx_buf).unwrap());
+    // gpio::set_high("81");
+    // thread::sleep(Duration::from_millis(100));
+
+    // let mut tx_buf_wr = [0x18, 0x08, 0x91, 0x35];
+    // let mut rx_buf_wr = [0; 4];
+    // gpio::set_low("81");
+    // thread::sleep(Duration::from_micros(200));
+    // println!("{:?}", full_duplex(&mut spi, &mut tx_buf_wr, &mut rx_buf_wr).unwrap());
+    // gpio::set_high("81");
+    // thread::sleep(Duration::from_millis(100));
+    // loop {
+    //     gpio::set_low("81");
+    //     thread::sleep(Duration::from_micros(200));
+    //     println!("{:?}", full_duplex(&mut spi, &mut tx_buf, &mut rx_buf).unwrap());
+    //     gpio::set_high("81");
+    //     thread::sleep(Duration::from_millis(100));
+    // }
